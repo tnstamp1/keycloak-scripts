847c798ef6e41a8888785bd50844e564f52ed6f7
Terence Stamp <terence_stamp@hotmail.com>
2018-11-15 20:42:08 +0000

    amended typos

--- get-kc-crt.sh
+++ get-kc-crt.sh
@@ -25,8 +25,7 @@ curl \
   -s \
   -H "Authorization: Bearer ${ACCESS_TOKEN}" \
   -H "Content-Type: application/json" \
-  http://localhost:8080/auth/admin/realms/${REALM}/keys | jq '.keys[].certificate' -r
-#  http://localhost:8080/auth/realms/${REALM}/keys | jq '.keys[].certificate' -r | grep -v "^null" 
+  http://localhost:8080/auth/admin/realms/${REALM}/keys | jq '.keys[].certificate' -r | grep -v null
 echo "-----END CERTIFICATE-----"
   
 # and then just format the certifcate nice .. i.e. 63 char + \n .. and BEGIN and END CERTIFICATE tags
